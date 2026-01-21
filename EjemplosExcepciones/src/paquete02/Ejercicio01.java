039096555b2f8d23e5245feb9f86fad07483e711
Anthony <tonyxiz10@gmail.com>
2026-01-21 14:34:01 -0500

    commit

--- EjemplosExcepciones/src/paquete02/Ejercicio01.java
+++ EjemplosExcepciones/src/paquete02/Ejercicio01.java
@@ -14,40 +14,54 @@ import java.util.Scanner;
 public class Ejercicio01 {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
         int valor1 = ingresarValor();
-        int valor2 = ingrsarValor();
-        int resultado = obtenerDatos();
+        int valor2 = ingresarValor();
+        int suma = obtenerSuma(valor1, valor2);
+        String reporte = obtenerReporte(valor1, valor2, suma);
+        imprimir(reporte);
     }
 
     public static int ingresarValor() {
-        Scanner entrada = new Scanner(System.in);
-
         boolean bandera = true;
         int valor = 0;
-
         while (bandera) {
             try {
-                System.out.println("Ingrese valor 1 a operar: ");
+                Scanner entrada = new Scanner(System.in);
+                System.out.println("Ingrese valor a operar: ");
                 valor = entrada.nextInt();
-                if(valor %2==0 && valor >= 0){
-                    bandera = false;
-                }
                 if (valor < 0) {
                     throw new Exception("Error número negativo");
                 }
                 if (valor %2!= 0) {
                     throw new Exception("Error número impar");
                 }
+                bandera = false;
             } 
-             catch (InputMismatchException e) {
-                System.out.printf("(InputMismatchException) Ocurrió una "
-                        + "excepción %s\n", e);
-            } catch (Exception e) {
-                System.out.printf("Ocurrió una excepción %s\n", e);
+             catch (InputMismatchException e) { // otro tipo =! int
+            System.out.printf("(InputMismatchException) Ocurrió una "
+                    + "excepción %s\n", e);
+            } catch (Exception e) { // impar o menor a 0
+            System.out.printf("Ocurrió una excepción %s\n", e);
             }
         }
 
         return valor;
+    }
+    
+    public static int obtenerSuma(int a, int b){
+        int suma;
+        suma = a + b;
+        return suma;
+    }
+    
+    public static String obtenerReporte(int a, int b, int c){
+        String mensaje;
+        mensaje = String.format("El resultado de la suma de %d y %d es: %d\n", 
+                a, b, c);
+        return mensaje;
+    }
+    
+    public static void imprimir(String a){
+        System.out.printf(a);
     }
 }
